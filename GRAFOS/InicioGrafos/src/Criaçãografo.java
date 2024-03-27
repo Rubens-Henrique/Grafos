1d4d76d0ba42d005d5c508aa24779eb7f8851535
Rubens-Henrique <128429640+Rubens-Henrique@users.noreply.github.com>
2024-03-27 20:20:22 -0300

    Add files via upload

---
+++ GRAFOS/InicioGrafos/src/Criaçãografo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public static void Criaçãografo() {
+    
+    Scanner sc = new Scanner(System.in);
+
+
+    //variáveis
+    int tipo=0;
+    int numeroVertice=0;
+
+    System.out.println("Quantos vértices deseja ter? ");
+    numeroVertice=sc.nextInt();
+    while (numeroVertice<=0) 
+    {   System.out.println("Digite um valor acima de 0 ");
+        numeroVertice=sc.nextInt();  
+    }
+    System.out.println("Que tipo de grafo deseja montar? Digite : 1- Grafo direcionado , 2- Grafo não direcionado ");
+    tipo=sc.nextInt();
+
+    System.out.print("Digite os vértices que tem relação entre si , exemplo : 1,2/ 3,4 /2,4");
+    int origem =sc.nextInt();
+    int destino =sc.nextInt();
+
+//chamada do aruqivo
+
+
+    MatrizAdjCriacao(numeroVertice);
+    NomeVertice(numeroVertice);
+    AddAresta(origem,destino);
+
+
+    
+
+
+
+
+
+    
+}
---
+++ GRAFOS/InicioGrafos/src/Operacao.java
@@ -0,0 +1,51 @@
+public class Operacao {
+    
+
+
+    // Testar se o grafo é simples,
+public static boolean grafosimples(int[][] matrizAdj) 
+   
+{
+
+
+    int numeroVertice = matrizAdj.length;
+    for (int i=0;i<numeroVertice ;i++)
+    {   for (int j=0;j<numeroVertice ;j++)
+             if (matrizAdj[i][j]>1 || (i==j && matrizAdj[i][i]==1))
+                  {  return false; } 
+    } 
+    
+    return true; // Grafo é simples
+
+    }
+
+    //testar se o grafo é regular
+
+
+    //testar se o grafo é completo
+
+    public static boolean grafoCompleto(int[][] matrizAdj+"RETORNO DO GRAFO SIMPLES") 
+    {   
+        if (grafosimples=true)
+        { for (int i = 0; i < matrizAdj.length; i++) {
+        for (int j = 0; j < matrizAdj[i].length; j++) {
+            if (i != j && matrizAdj[i][j] != 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+    } else {
+    return false; // Se o grafo não for simples, não pode ser completo
+    }
+    }
+
+
+
+    //testar se é bipartido 
+    
+
+
+
+
+}
